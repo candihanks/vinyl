9d132aeee48e3181f3c57bc53a4bfb97d8277c51
Carl Taylor <carltaylor43uk@gmail.com>
2017-06-02 08:52:52 +0100

    added audio fragment and audio player

--- app/src/main/java/com/carltaylordev/recordlisterandroidclient/UserInterface/AudioFragment.java
+++ app/src/main/java/com/carltaylordev/recordlisterandroidclient/UserInterface/AudioFragment.java
@@ -1,16 +1,41 @@
 package com.carltaylordev.recordlisterandroidclient.UserInterface;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.carltaylordev.recordlisterandroidclient.Logger;
+import com.carltaylordev.recordlisterandroidclient.Media.FileManager;
 import com.carltaylordev.recordlisterandroidclient.R;
 import com.carltaylordev.recordlisterandroidclient.RecordSessionManager;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class AudioFragment extends android.support.v4.app.Fragment implements RecordSessionManager.UpdateInterface  {
 
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static String mFileName = null;
+
+    private MediaRecorder mRecorder = null;
+    private MediaPlayer mPlayer = null;
+
+    private Button mRecordButton = null;
+    private Button  mPlayButton = null;
+
+    private boolean mStartRecording = true;
+    private boolean mStartPlaying = true;
 
     /**
      * Constructors
@@ -23,6 +48,9 @@ public class AudioFragment extends android.support.v4.app.Fragment implements Re
         return new AudioFragment();
     }
 
+    /**
+     *  Fragment LifeCycle
+     */
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,8 +58,145 @@ public class AudioFragment extends android.support.v4.app.Fragment implements Re
 
         View rootView = inflater.inflate(R.layout.audio_fragment, container, false);
 
+        setupAudioRecorder();
+        setupButtons(rootView);
+
         return rootView;
     }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mRecorder != null) {
+            mRecorder.release();
+            mRecorder = null;
+        }
+
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    /**
+     *  Setup
+     */
+
+    private void setupAudioRecorder() {
+        FileManager.createDirectory(FileManager.getRootSoundClipsPath());
+        try {
+            File temp = FileManager.createTempFileOnDisc(".3gp");
+            mFileName = temp.getAbsolutePath();
+        } catch (Exception e) {
+            Logger.logMessage("BOOM");
+        }
+    }
+
+    private void setupButtons(View view) {
+        mRecordButton = (Button) view.findViewById(R.id.recordButton);
+        mPlayButton = (Button) view.findViewById(R.id.playButton);
+
+        mRecordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRecord(mStartRecording);
+                if (mStartRecording) {
+                   mRecordButton.setText("Stop recording");
+                } else {
+                    mRecordButton.setText("Start recording");
+                }
+                mStartRecording = !mStartRecording;
+            }
+        });
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPlay(mStartPlaying);
+                if (mStartPlaying) {
+                    mPlayButton.setText("Stop playing");
+                } else {
+                    mPlayButton.setText("Start playing");
+                }
+                mStartPlaying = !mStartPlaying;
+            }
+        });
+    }
+
+
+    /**
+     *  Audio Controls
+     */
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    private void onPlay(boolean start) {
+        if (start) {
+            startPlaying();
+        } else {
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     /**
      *  RecordSessionManager Interface
