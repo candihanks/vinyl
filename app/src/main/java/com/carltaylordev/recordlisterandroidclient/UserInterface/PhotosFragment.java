e1e3549fc4f5062d0fda7cadec8c4527c48a552d
Carl Taylor <carltaylor43uk@gmail.com>
2017-06-02 12:51:24 +0100

    removed import

--- app/src/main/java/com/carltaylordev/recordlisterandroidclient/UserInterface/PhotosFragment.java
+++ app/src/main/java/com/carltaylordev/recordlisterandroidclient/UserInterface/PhotosFragment.java
@@ -5,7 +5,6 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
