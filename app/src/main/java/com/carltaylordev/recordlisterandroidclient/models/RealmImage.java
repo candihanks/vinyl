22cce65fccd346412a7d46fd86c1390a9e4f3277
Carl Taylor <carltaylor43uk@gmail.com>
2017-06-08 12:31:47 +0100

    adding load of reduced size image before converting to thumb

--- app/src/main/java/com/carltaylordev/recordlisterandroidclient/models/RealmImage.java
+++ app/src/main/java/com/carltaylordev/recordlisterandroidclient/models/RealmImage.java
@@ -49,8 +49,34 @@ public class RealmImage extends RealmObject {
     }
 
     public void rehydrate() {
-        Bitmap tempThumb = BitmapFactory.decodeFile(path);
-        thumb = convertToThumb(tempThumb);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        // First decode with inJustDecodeBounds=true to check dimensions
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, 100, 100);
+        // Decode bitmap with inSampleSize now set
+        options.inJustDecodeBounds = false;
+        thumb = convertToThumb(BitmapFactory.decodeFile(path, options));
+    }
+
+    private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
     }
 
     public void freeUpMemory() {
